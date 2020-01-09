ce253b94444eeb062422d7aad40bc1c56c95ad63
Beatriz Lopez Del Moral Garcia Jimenez <beatriz.lopez@live.u-tad.com>
2020-01-09 11:22:30 +0100

    Initial Commit

---
+++ Sofuber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sofuber
+//
+//  Created by Beatriz Lopez Del Moral Garcia Jimenez on 9/1/20.
+//  Copyright © 2020 BEATRIZ LOPEZ DEL MORAL GARCIA JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
