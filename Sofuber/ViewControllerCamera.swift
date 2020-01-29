a61943b308ec65679dd2af891decccb2122673f6
Beatriz Lopez Del Moral Garcia Jimenez <beatriz.lopez@live.u-tad.com>
2020-01-29 12:55:42 +0100

    TextToSpeechON

--- Sofuber/ViewControllerCamera.swift
+++ Sofuber/ViewControllerCamera.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import TesseractOCR
+import AVFoundation
 
 class ViewControllerCamera: UIViewController, G8TesseractDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
  
     let imagePicker: UIImagePickerController = UIImagePickerController()
     @IBOutlet weak var textImage: UITextView!
     @IBOutlet weak var image: UIImageView!
-    
+    @IBOutlet weak var segmentlenguage: UISegmentedControl!
    
     
     override func viewDidLoad() {
@@ -40,7 +41,9 @@ class ViewControllerCamera: UIViewController, G8TesseractDelegate,UIImagePickerC
     func progressImageRecognition(for tesseract: G8Tesseract!) {
         print("Recognition Progress\(tesseract.progress)&")
     }
-    @IBAction func takePhoto(_ sender: AnyObject){
+    
+    @IBAction func takePhoto(_ sender: UIButton){
+        print("paso1")
         //Si la camara está activa
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             if UIImagePickerController.availableCaptureModes(for: .rear) != nil {
@@ -49,8 +52,35 @@ class ViewControllerCamera: UIViewController, G8TesseractDelegate,UIImagePickerC
                 imagePicker.sourceType = .camera
                 imagePicker.cameraCaptureMode = .photo
                 present(imagePicker,animated: true,completion: nil)
+                print("paso")
         }
     }
+    }
+    
+    @IBAction func textToSpeech(_ sender: UIButton){
+        var lang: String = "en-US"
+        switch segmentlenguage.selectedSegmentIndex {
+        case 0:
+            lang = "en-US"
+        case 1:
+            lang = "es-ES"
+        default:
+            lang = "es-ES"
+        }
+        self.readMe(myText: textImage.text! , myLang: lang)
+    }
+    
+    func readMe(myText: String, myLang: String){
+        let utterance = AVSpeechUtterance(string: myText)
+        //seleccionamos la entonación del idioma
+        utterance.voice = AVSpeechSynthesisVoice(language: myLang)
+        //Indico la velocidad de lectura
+        utterance.rate = 0.5
+        
+        let synthesizer = AVSpeechSynthesizer()
+        //Le pasamos al sistetizador nuestro texto con la configuración y
+        //se escuchará
+        synthesizer.speak(utterance)
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -58,8 +88,11 @@ class ViewControllerCamera: UIViewController, G8TesseractDelegate,UIImagePickerC
             
             image.image = imageselected
         }
+        //Quitamos la animación de la camara
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
